f7b4e0ada36e7ad9dace8366f272abc22edb9742
JimHinke <jimmiehinke@gmail.com>
2022-10-14 09:56:29 +0200

    la till en fjärde kommentar

--- PhoneListApp3/Program.cs
+++ PhoneListApp3/Program.cs
@@ -111,7 +111,7 @@ namespace PhoneListApp
             Console.WriteLine($"Namn: {fNamn} {eNamn}"); //kommentar2
             Console.WriteLine($"Adress: {adress}"); //Kommentar 3
             Console.WriteLine($"Telefonnummer: {telefonnummer}"); //kommentar 4
-            Console.WriteLine("");
+            Console.WriteLine(""); //kommentar 5
         }
 
         public void PrintForEfter()
